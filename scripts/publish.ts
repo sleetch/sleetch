1ab3c1608846dba31955a34870a966c192daf4e1
Tornado Softwares <tornadogamesfrance@gmail.com>
2026-08-06 01:43:32 +0200

    Improved publich script conclusion logging and added github workflow

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -14,19 +14,23 @@ for await (const file of new Glob("**/package.json").scan({ cwd: root })) {
 }
 
 const release_plan = await get_release_plan(root);
+const releases = release_plan.releases.filter(
+  (r) => r.oldVersion !== r.newVersion,
+);
 
-for (const release of release_plan.releases) {
-  if (release.oldVersion !== release.newVersion) {
-    console.log(`Publishing : ${release.name}@${release.newVersion}`);
-    const result = Bun.spawnSync({
-      cmd: ["bun", "publish", "--no-git-checks", "--access", "public"],
-      cwd: packages_to_cwd.get(release.name),
-      stdout: "inherit",
-      stderr: "inherit",
-    });
+for (const release of releases) {
+  console.log(`Publishing : ${release.name}@${release.newVersion}`);
+  const result = Bun.spawnSync({
+    cmd: ["bun", "publish", "--no-git-checks", "--access", "public"],
+    cwd: packages_to_cwd.get(release.name),
+    stdout: "inherit",
+    stderr: "inherit",
+  });
 
-    if (!result.success) {
-      process.exit(result.exitCode ?? 1);
-    }
+  if (!result.success) {
+    process.exit(result.exitCode ?? 1);
   }
 }
+
+if (releases.length == 0) console.log("Nothing to publish..");
+else console.log(`Publisheddd ${releases.length} packages.`);

9a55672ad2757202c2980ea80f120d9485f66f08
Tornado Softwares <tornadogamesfrance@gmail.com>
2026-08-12 01:02:45 +0200

    Lerp eyes mouse tracking

--- applications/website/src/app/routes/eyes.tsx
+++ applications/website/src/app/routes/eyes.tsx
@@ -15,46 +15,56 @@ export default function Home() {
   const leftEyeRef = useRef<HTMLDivElement>(null);
   const leftEyeOriginRef = useRef<HTMLDivElement>(null);
 
+  const eyesRef = useRef([
+    { eye: rightEyeRef, origin: rightEyeOriginRef, angle: 0, offset: 20 },
+    { eye: leftEyeRef, origin: leftEyeOriginRef, angle: 0, offset: 20 },
+  ]);
+
+  const lerp = (start: number, end: number, amount: number) => {
+    return start + (end - start) * amount;
+  };
+
   useEffect(() => {
-    const rightEye = rightEyeRef.current;
-    const rightOrigin = rightEyeOriginRef.current;
+    let frame: number;
+    const animate = () => {
+      for (const data of eyesRef.current) {
+        const eye = data.eye.current;
+        const origin = data.origin.current;
+        if (eye && origin) {
+          if (cursor_position.x == 0 && cursor_position.y == 0) {
+            data.offset = lerp(data.offset, 50, 0.1);
+          } else {
+            data.offset = lerp(data.offset, 20, 0.1);
+            const origin_rect = origin.getBoundingClientRect();
+            const origin_position = { y: origin_rect.top, x: origin_rect.left };
 
-    const leftEye = leftEyeRef.current;
-    const leftOrigin = leftEyeOriginRef.current;
+            const origin_to_eye_vector = {
+              x: 1,
+              y: 0,
+            };
+            const origin_to_eye_vector_norm = Math.sqrt(origin_to_eye_vector.x ** 2 + origin_to_eye_vector.y ** 2);
 
-    let offset = 20;
+            const origin_to_cursor_vector = { x: cursor_position.x - origin_position.x, y: cursor_position.y - origin_position.y };
+            const origin_to_cursor_vector_norm = Math.sqrt(origin_to_cursor_vector.x ** 2 + origin_to_cursor_vector.y ** 2);
 
-    if (rightEye && rightOrigin && leftEye && leftOrigin) {
-      const eyes = [
-        { eye: rightEye, origin: rightOrigin },
-        { eye: leftEye, origin: leftOrigin },
-      ];
-      if (cursor_position) {
-        if (cursor_position.x == 0 && cursor_position.y == 0) offset = 50;
-        for (const { eye, origin } of eyes) {
-          const origin_rect = origin.getBoundingClientRect();
-          const origin_position = { y: origin_rect.top, x: origin_rect.left };
+            const eye_scalar_cursor = origin_to_eye_vector.x * origin_to_cursor_vector.x + origin_to_eye_vector.y * origin_to_cursor_vector.y;
 
-          const origin_to_eye_vector = {
-            x: 1,
-            y: 0,
-          };
-          const origin_to_eye_vector_norm = Math.sqrt(origin_to_eye_vector.x ** 2 + origin_to_eye_vector.y ** 2);
+            const scalar_angle = Math.acos(eye_scalar_cursor / (origin_to_eye_vector_norm * origin_to_cursor_vector_norm));
+            const det = origin_to_eye_vector.x * origin_to_cursor_vector.y - origin_to_eye_vector.y * origin_to_cursor_vector.x;
+            let new_angle = det >= 0 ? -scalar_angle : scalar_angle;
+            const diff = Math.round((data.angle - new_angle) / (Math.PI * 2)) * Math.PI * 2; // AI Generated, prevents multiple turn of the eye..
+            data.angle = lerp(data.angle, new_angle + diff, 0.1);
+          }
 
-          const origin_to_cursor_vector = { x: cursor_position.x - origin_position.x, y: cursor_position.y - origin_position.y };
-          const origin_to_cursor_vector_norm = Math.sqrt(origin_to_cursor_vector.x ** 2 + origin_to_cursor_vector.y ** 2);
-
-          const eye_scalar_cursor = origin_to_eye_vector.x * origin_to_cursor_vector.x + origin_to_eye_vector.y * origin_to_cursor_vector.y;
-
-          const scalar_angle = Math.acos(eye_scalar_cursor / (origin_to_eye_vector_norm * origin_to_cursor_vector_norm));
-          const det = origin_to_eye_vector.x * origin_to_cursor_vector.y - origin_to_eye_vector.y * origin_to_cursor_vector.x;
-          const angle = det >= 0 ? -scalar_angle : scalar_angle;
-          eye.style.top = `${50 + (50 - offset) * -Math.sin(angle)}%`;
-          eye.style.left = `${50 + (50 - offset) * Math.cos(angle)}%`;
+          eye.style.top = `${50 + (50 - data.offset) * -Math.sin(data.angle)}%`;
+          eye.style.left = `${50 + (50 - data.offset) * Math.cos(data.angle)}%`;
         }
       }
-    }
-  }, [rightEyeOriginRef, rightEyeRef, leftEyeRef, leftEyeOriginRef, cursor_position]);
+      frame = requestAnimationFrame(animate);
+    };
+    animate();
+    return () => cancelAnimationFrame(frame);
+  }, [eyesRef, cursor_position]);
 
   return (
     <main
